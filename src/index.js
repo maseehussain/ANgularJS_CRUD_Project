20f13f6b6dd596261a6f2aa11e944aaecb9b3b4d
Masee Hussain <hussainmasee@gmail.com>
2018-07-20 11:00:18 +0100

    added html layout in index.html

--- src/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("app").innerHTML = `
-<h1>Angualr JS Project
-</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
